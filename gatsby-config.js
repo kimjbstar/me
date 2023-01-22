d7069d08dfe1cf4c2bead7e438f358fe8a16f7bb
JongBeom Kim <kimjbstar@gmail.com>
2023-01-22 15:13:41 +0900

    remove all

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('gatsby').GatsbyConfig} */
-module.exports = {
-  siteMetadata: {
-    title: `new`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-styled-components"]
-};
