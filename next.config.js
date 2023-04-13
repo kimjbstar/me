23a2701a908191790ab1010f1ce84300fdee6401
JongBeom Kim <kimjbstar@gmail.com>
2023-04-14 01:17:39 +0900

    feat: prefix 경로 변경

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isDebug = process.env.NODE_ENV !== "production"
-const repository = "kimjbstar-blog"
-
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix:
--- src/config/config.ts
+++
@@ -1,4 +0,0 @@
-export const prefix =
-  process.env.NODE_ENV === "production"
-    ? "https://kimjbstar.github.io/blog"
-    : ""
