b5a7dda4cdc278b698930519f67372375a9c1c2e
JongBeom Kim <kimjbstar@gmail.com>
2023-04-17 23:20:08 +0900

    feat: font roboto로 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from "next/app"
 import "../styles/globals.css"
 import "../styles/markdown.css"
-import { Noto_Sans_KR } from "@next/font/google"
+import { Roboto } from "@next/font/google"
 import { prefix } from "@/config/config"
 import { AppProvider } from "@/context/context"
 import Head from "next/head"
 
-const defaultfont = Noto_Sans_KR({ subsets: ["latin"], weight: "100" })
+const defaultfont = Roboto({ weight: "300", subsets: ["greek"] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
