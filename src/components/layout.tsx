59ec8077eebf3dc7a02aa77536530115ea4b9999
JongBeom Kim <kimjbstar@gmail.com>
2023-04-17 22:00:19 +0900

    feat: layout reponseive padding 조정

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout(props: React.PropsWithChildren<Props>) {
   return (
     <>
       <Header></Header>
-      <div className="lg:p-20 sm:p-5">{props.children}</div>
+      <div className="md:p-16 p-4">{props.children}</div>
     </>
   )
 }
