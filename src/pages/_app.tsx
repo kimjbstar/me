8288fa5e40d89c37ff8fdfe1cbc1292095f710d0
JongBeom Kim <kimjbstar@gmail.com>
2023-04-20 14:24:31 +0900

    feat: ga 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Roboto } from "@next/font/google"
 import { prefix } from "@/config/config"
 import { AppProvider } from "@/context/context"
 import Head from "next/head"
+import Script from "next/script"
 
 const defaultfont = Roboto({ weight: "300", subsets: ["greek"] })
 
@@ -18,6 +19,22 @@ export default function App({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
       </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            console.log('NEXT_PUBLIC_GA_TRACKING_ID','${process.env.NEXT_PUBLIC_GA_TRACKING_ID}')
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
       <AppProvider value={{ prefix }}>
         <main className={defaultfont.className}>
           <Component {...pageProps} />
