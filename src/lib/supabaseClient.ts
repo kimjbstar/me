5670b777af263d7e2bab6d882b70a4816d0671c7
JongBeom Kim <kimjbstar@gmail.com>
2023-04-20 14:48:13 +0900

    feat: supabase env명 변경

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const accessToken = process.env.SUPABASE_ACCESS_TOKEN!
-const host = process.env.SUPABASE_HOST!
+const accessToken = process.env.NEXT_PUBLIC_SUPABASE_ACCESS_TOKEN!
+const host = process.env.NEXT_PUBLIC_SUPABASE_HOST!
 const supabase = createClient(host, accessToken)
 export default supabase
