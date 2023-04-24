df6faa7e9eca24eee6f220d4f4ff34be70b9ea99
JongBeom Kim <kimjbstar@gmail.com>
2023-04-24 13:18:40 +0900

    feat: google analytics 스크립트 컴포넌트 분리

---
+++ src/components/google-analytics.tsx
@@ -0,0 +1,25 @@
+import Script from "next/script"
+
+export const GoogleAnalytics = () => (
+  <>
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+    />
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+      }}
+    />
+  </>
+)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { Roboto } from "@next/font/google"
 import { prefix } from "@/config/config"
 import { AppProvider } from "@/context/context"
 import Head from "next/head"
-import Script from "next/script"
+import { GoogleAnalytics } from "@/components/google-analytics"
 
 const defaultfont = Roboto({ weight: "300", subsets: ["greek"] })
 
@@ -19,21 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
       </Head>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-        }}
-      />
+      <GoogleAnalytics></GoogleAnalytics>
       <AppProvider value={{ prefix }}>
         <main className={defaultfont.className}>
           <Component {...pageProps} />
